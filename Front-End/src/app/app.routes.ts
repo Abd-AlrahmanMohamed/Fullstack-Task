78454a50cdf8fe9394cfe6c05a292f6151bdfa88
Abdelrahman Mohamed <156607221+Abd-AlrahmanMohamed@users.noreply.github.com>
2025-09-19 00:42:52 +0300

    Update app.routes.ts

--- Front-End/src/app/app.routes.ts
+++ Front-End/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from '../Components/Register/Register.component';
 import { LoginComponent } from '../Components/Login/Login.component';
-import { LiveChatComponent } from '../Components/LiveChat/LiveChat.component';
+import { ProductComponent } from '../Components/Product/Product.component';
+import { AddProductComponent} from '../Component/Product/AddProduct/AddProduct.component';
 
 export const routes: Routes = [
   { path: 'adduser', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', component: LiveChatComponent },
+  { path: '', component: ProductComponent },
+  {path: 'add-product', component: AddProductComponent}
 ];
+
